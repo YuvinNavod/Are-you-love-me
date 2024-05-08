c9c500f86596a9175934a9343d42c458eeb48ec5
Yuvin Navod <137191227+YuvinNavod@users.noreply.github.com>
2024-05-08 20:41:43 +0530

    update

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 
 
 function makeNoButtonAppear() {
-    var top = Math.random() * 400;
+    var top = getRandomNumber(400, 500);
 
-    var left = Math.random() * 400;
+    var left =  getRandomNumber(200, 1000);
+
     document.getElementById("no").style.position = "absolute";
-  
     document.getElementById("no").style.top = top + "px";
     document.getElementById("no").style.left = left + "px";
     document.getElementById("no").style.display = "block";
-
-
    
   }
+
+  function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+
 function appearAfterDelay() {
     setTimeout(makeNoButtonAppear, Math.random() * 2000);
 
